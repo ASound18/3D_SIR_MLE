30a87e080213c931180623ae944f931685c4949f
ASound18 <g.t.tongchuan@gmail.com>
2018-01-15 14:16:26 +0800

    add tab2

--- server.R
+++ server.R
@@ -8,6 +8,8 @@ values = sort(unique(grid_df$measurement_noise_volatility_values))
 
 shinyServer(function(input, output, session) {
   
+  ### ~~~~ Tab1 ~~~~ ###
+  
   output$plot <- renderPlotly({
     input_delta = input$delta
     if(input_delta == 0) input_delta = 1e-4
@@ -81,6 +83,66 @@ shinyServer(function(input, output, session) {
     value = grid_df$measurement_noise_volatility_values[grid_df$logLik==max_Loglik_G]
     value = value[!is.na(value)]
     updateSliderInput(session, "delta", label = "Measurement Noise Volatility (Delta):", min = 0, max = max(values), step = 0.005, value = value)
+  })
+  
+  
+  ### ~~~~ Tab2 ~~~~ ###
+  
+  output$plot2 <- renderPlotly({
+    
+    withProgress(message = 'Rendering plot', value = 0.6, {
+      pnts_x = rep(NA, length(values))
+      pnts_y = rep(NA, length(values))
+      pnts_z = rep(NA, length(values))
+      
+      x = unique(grid_df$asset_drift_values)
+      y = unique(grid_df$asset_volatility_values)
+      
+      for(i in 1:length(values)) {
+        temp_df = grid_df[grid_df$measurement_noise_volatility_values == values[i],]
+        z = t(matrix(temp_df$logLik, nrow = length(x)))
+        assign(paste0("z",i), z)
+        
+        maxLogLik = max(temp_df$logLik, na.rm = TRUE)
+        pnts_z[i] = maxLogLik
+        temp_x = temp_df$asset_drift_values[temp_df$logLik == maxLogLik]
+        pnts_x[i] = temp_x[!is.na(temp_x)]
+        temp_y = temp_df$asset_volatility_values[temp_df$logLik == maxLogLik]
+        pnts_y[i] = temp_y[!is.na(temp_y)]
+      }
+      
+      
+      plot_ly(showscale = FALSE) %>% 
+        add_surface(x = x, y = y, z = z1, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z2, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z3, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z4, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z5, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z6, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z7, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z8, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z9, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z10, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z11, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z12, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z13, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z14, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z15, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z16, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z17, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z18, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z19, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z20, opacity = 0.9) %>%
+        add_surface(x = x, y = y, z = z21, opacity = 0.9) %>%
+        add_trace(#data = temp_df, 
+          x = pnts_x,
+          y = pnts_y,
+          z = pnts_z,
+          mode = "lines+markers", type = "scatter3d") %>%
+        layout(autosize = F, width = 800, height = 800)
+    })
+    
+    
   })
   
 })
--- ui.R
+++ ui.R
@@ -10,37 +10,47 @@ shinyUI(fluidPage(
   
   titlePanel("Particle Filtering-based Maximum Likelihood Estimation"),
   
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("delta", "Measurement Noise Volatility (Delta):", min = 0, max = max(values), step = 0.005, value = 0, animate=TRUE),
-      br(),br(),
-      fluidRow(
-        column(12, h4(strong("Global Optimum"))),
-        column(12, h5(strong("Optimal Asset Drift:"))),
-        column(12, textOutput("maxPointG_x")),
-        column(12, h5(strong("Optimal Asset Volatility:"))),
-        column(12, textOutput("maxPointG_y")),
-        column(12, h5(strong("Optimal Delta:"))),
-        column(6, textOutput("maxPointG_delta")),
-        column(6, actionButton("optimalDeltaButton", "View by this optimal Delta", icon = icon("eye"))),
-        column(12, h5(strong("Optimal Log-Likelihood:"))),
-        column(12, textOutput("maxPointG_z"))
-      )
-    ),
+  tabsetPanel(
+    tabPanel("View By Single Layer",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("delta", "Measurement Noise Volatility (Delta):", min = 0, max = max(values), step = 0.005, value = 0, animate=TRUE),
+                 br(),br(),
+                 fluidRow(
+                   column(12, h4(strong("Global Optimum"))),
+                   column(12, h5(strong("Optimal Asset Drift:"))),
+                   column(12, textOutput("maxPointG_x")),
+                   column(12, h5(strong("Optimal Asset Volatility:"))),
+                   column(12, textOutput("maxPointG_y")),
+                   column(12, h5(strong("Optimal Delta:"))),
+                   column(6, textOutput("maxPointG_delta")),
+                   column(6, actionButton("optimalDeltaButton", "View by this optimal Delta", icon = icon("eye"))),
+                   column(12, h5(strong("Optimal Log-Likelihood:"))),
+                   column(12, textOutput("maxPointG_z"))
+                 )
+               ),
+               
+               mainPanel(
+                 plotlyOutput("plot"),
+                 fluidRow(
+                   column(12, h4(strong("Local Optimum"))),
+                   column(12, h5(strong("Optimal Asset Drift:"))),
+                   column(12, textOutput("maxPoint_x")),
+                   column(12, h5(strong("Optimal Asset Volatility:"))),
+                   column(12, textOutput("maxPoint_y")),
+                   column(12, h5(strong("Optimal Log-Likelihood:"))),
+                   column(12, textOutput("maxPoint_z"))
+                 )
+                 
+               )
+             )
+            ),
     
-    mainPanel(
-      plotlyOutput("plot"),
-      fluidRow(
-        column(12, h4(strong("Local Optimum"))),
-        column(12, h5(strong("Optimal Asset Drift:"))),
-        column(12, textOutput("maxPoint_x")),
-        column(12, h5(strong("Optimal Asset Volatility:"))),
-        column(12, textOutput("maxPoint_y")),
-        column(12, h5(strong("Optimal Log-Likelihood:"))),
-        column(12, textOutput("maxPoint_z"))
-      )
-      
-    )
+    tabPanel("View All Layers", 
+             fluidRow(column(8, offset=3,  plotlyOutput("plot2")))
+             )
   )
+  
+  
   
 ))
